0e7106dff2baf8ca6cd4055131113263857053a4
nitokrisalpha <nitokrisalpha@outlook.com>
2026-09-01 22:31:34 +0800

    FirstCommit

--- src/main/kotlin/com/nitokrisalpha/service/JavService.kt
+++ src/main/kotlin/com/nitokrisalpha/service/JavService.kt
@@ -121,7 +121,7 @@ class JavService {
         }
         repository.save(javWork)
         log.info("开始下载")
-        if (javWork.hash != null && downloader.exists(javWork.hash) && javWork.status != Status.MOVED) {
+        if (javWork.hash != null && downloader.exists(javWork.hash)) {
             log.info("该种子已存在：${javWork.magnet}")
             downloader.start(javWork.hash)
         } else {
